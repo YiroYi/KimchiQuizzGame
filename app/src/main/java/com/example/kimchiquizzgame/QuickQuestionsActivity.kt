64849d6251177cc6a86199f2887c7ebf855291e7
Yiro Yi <yiroyi@live.com>
2022-08-28 13:55:52 +0900

    Creating new Activity (New View Page)

---
+++ app/src/main/java/com/example/kimchiquizzgame/QuickQuestionsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kimchiquizzgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuickQuestionsActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_quick_questions)
+  }
+}
